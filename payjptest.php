8f9ee4a98a70a000578b82071842204543934a6a
YD-k-madoguchi <100896087+YD-k-madoguchi@users.noreply.github.com>
2022-03-10 15:01:19 +0900

    pullした分の変更

--- payjptest.php
+++ payjptest.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    ああああ
-<?php    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php    
 
 
 
@@ -17,7 +16,17 @@
 
 
 ?>
-    <script type="text/javascript" src="https://js.pay.jp/v1/"></script>
 
+<form>
+    カード番号<input type="text" name="card_num"><br>
+    月<input type="text" name="exp_month">/
+    年<input type="text" name="exp_year"><br>
+    CVC<input type="text" name="cvc"><br>
+    金額<input type="text" name="money">
+    
+    
+</form>
+
+<script type="text/javascript" src="https://js.pay.jp/v1/"></script>
 </body>
 </html>
