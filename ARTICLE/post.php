86d14d795949783142504364f64e71d833135a8f
Yuji Matsunaga <100896497+YujiMatsunaga@users.noreply.github.com>
2022-03-14 18:27:54 +0900

    3/14,18:30

---
+++ ARTICLE/post.php
@@ -0,0 +1,16 @@
+<?php require_once("head.php") ?>
+
+<?php
+  // require_once("header.php");
+?>
+
+<main>
+  <div class="container">
+    <h2 class="text-center mt-5">記事ページ</h2>
+    <?php require_once("postInput.php") ?>
+  </div>
+</main>
+
+<?php
+  // require_once("footer.php");
+?>
