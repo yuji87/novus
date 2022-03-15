4c7da2ec5e14a28c7bf7daeef5d8731660ff2adc
YD-k-madoguchi <100896087+YD-k-madoguchi@users.noreply.github.com>
2022-03-15 10:15:03 +0900

    3/15commit１

---
+++ question/question_comp.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>質問投稿完了</title>
+</head>
+<body>
+
+<?php
+
+?>
+
+<div>投稿完了</div>
+<br>
+<div>質問の投稿が完了しました</div>
+
+<a href="http://localhost/qandasite/login_top.php">質問へ（仮でTOPリンク）</a>
+<a href="http://localhost/qandasite/login_top.php">TOP</a>
+
+<?php
+
+
+
+// echo "題名<br>";
+// echo $_POST['title'];
+// echo "カテゴリ：".$_POST['category'] ."<br>";
+// echo "本文<br>";
+// echo $_POST['message'];
+
+?>
+</body>
+</html>
