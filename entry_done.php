91ddcf3d3edf1f5dcaf17260f7bd809b1207edd4
korie28 <Rieko.Terada@kusurinomadoguchi.co.jp>
2022-03-16 09:31:57 +0900

    ９：３１

--- entry_done.php
+++ entry_done.php
@@ -49,7 +49,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="2.css" />
+    <link rel="stylesheet" type="text/css" href="css/top.css" />
     <title>会員登録完了画面</title>
 </head>
 
