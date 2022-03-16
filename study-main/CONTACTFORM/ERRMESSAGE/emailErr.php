fe940490f22de0a798696ddc11c1f3380cb596c3
Yuji Matsunaga <100896497+YujiMatsunaga@users.noreply.github.com>
2022-03-16 18:12:13 +0900

    ３月１６日

---
+++ study-main/CONTACTFORM/ERRMESSAGE/emailErr.php
@@ -0,0 +1,10 @@
+
+<?php if (isset($error["message"]) && $error["message"] === "blank") : ?>
+  <p class="ellMessage">何も入力されていません</p>
+<?php endif ?>
+<?php if (isset($error["message"]) && $error["message"] === "exceed") : ?>
+  <p class="ellMessage">お問い合わせ内容は1000文字以内にしてください</p>
+<?php endif ?>
+<?php if (isset($error["message"]) && $error["message"] === "Illegal") : ?>
+  <p class="ellMessage">メールアドレスが不正です</p>
+<?php endif ?>
