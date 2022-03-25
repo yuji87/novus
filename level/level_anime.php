ff392fe84cc27fd2ca91dd7a6a6bfc05efd07bd2
YD-k-madoguchi <100896087+YD-k-madoguchi@users.noreply.github.com>
2022-03-25 13:23:24 +0900

    レベルアニメーション修正
    ユーザーロジックのレベル引き出し部分で、持ってくるステータスの増加
    JS、CSSのパス修正など、不要部分の削除

--- level/level_anime.php
+++ level/level_anime.php
@@ -4,69 +4,58 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport">
   <title>Cherry Blossom DEMO</title>
-  <link rel="stylesheet" type="text/css" href="level_anime.css">
-  <link rel="stylesheet" type="text/css" href="../2.css" />
-
-  <link rel="stylesheet" href="ClickEffects-master/css/component.css"><!-- CSS読み込み -->
-  <script src="ClickEffects-master\js/classie.js"></script>
-  <script src="ClickEffects-master\js/modernizr.custom.js"></script>
+  <link rel="stylesheet" type="text/css" href="../../level/level_anime.css">
 </head>
 <body>
+  <div class="mx-auto" style="width: 200px;">
   <div class="cherry-blossom-container">
     <!-- レベルによる表示画像変更 -->
-    <img id="hero" class="col-4 d-block mx-auto" style="width: 100px; padding-left:27px; height: auto" src="../../level/img/22338667.png" alt="ないよ">
-    <div id="lv" class="col-4 d-block mx-auto">Lv.<span id="level"><?php echo $login_user['pre_level']; ?></span>
-      <!-- <span id="level"><?php// echo $user_data('pre_level') ?></span> -->
+    <img id="hero" class="col-4 d-block" style="width: 100px; height: auto" src="../../level/img/22338667.png" alt="ないよ">
+    <div id="lv" class="col-4 d-block">Lv.
+      <span id="level"><?php echo $user_data['pre_level'] ?></span>
     </div>
       <progress class=" d-block mx-auto" id="lifeBar" value="0" max="100" min="0" optimum="100"></progress>
       <div class="col-4 d-block  element js-animation" id="ohome_word">Congratulation!</div>
     <div id="bg"></div>
   </div>
-  
+  </div>
   
   <script type="text/javascript">
-    // 前回マイページ参照時からレベルが上がった分だけ処理を繰り返し
-    
-    let previousLevel = <?php echo $login_user['pre_level'] ?>;
-    let currentLevel = <?php echo $login_user['level'] ?>;
-    let previousExp = <?php echo $login_user['pre_exp'] ?>;
-    let currentExp = <?php echo $login_user['exp'] ?>;
-    
-    let lifeBar.value = previousExp
+    // 前提条件の定義
+    let previousLevel = <?php echo $user_data['pre_level'] ?>;
+    let currentLevel = <?php echo $user_data['level'] ?>;
+    let previousExp = <?php echo $user_data['pre_exp'] ?>;
+    let currentExp = <?php echo $user_data['exp'] ?>;
+    lifeBar.value = previousExp;
     let exExp = currentExp % 100;
     
-    // let previousLevel = 1;
-    // let currentLevel = 3;
-    // lifeBar.value = 0;
-    // let exExp = 70;
-    
+    // 前回開いたときからのレベルアップ処理
     function update() {
-      gameTimer = setTimeout(update, 4);
+      // 変数の定義
       let heroImg = document.getElementById('hero');
       let lifeBar = document.getElementById('lifeBar');
       let level = document.getElementById('level'); 
       let bg = document.getElementById('bg'); 
+      gameTimer = setTimeout(update, 4);
       lifeBar.value++;
     
       
-      // レベルアップ時のエフェクトを消去
+      // レベルアップ時エフェクトを除去（再使用可能にする）
       if(lifeBar.value == 95){
         document.getElementById("bg").classList.remove("before");
       }
-      // 経験値が100になったら、レベルが上がる
+      // 経験値100でレベルアップ
       if(lifeBar.value >= lifeBar.max){
         lifeBar.value = 0;
-        // レベルアップ時エフェクト起動(この後ループされ、上で消去される)
+        // レベルアップ時エフェクト起動
         document.getElementById("bg").classList.add("before");
         level.innerHTML++;
-        // document.getElementById("level").classList.add("s-fs-l");
         
         // 一定レベルを超えたら表示画像が変更される
         if(level.innerHTML >= 5){
           document.getElementById("hero").src='../../level/img/22503431.png';
-          // document.getElementById("hero").classList.add("cbutton--effect-stana");
         } 
         if(level.innerHTML >= 10){
           document.getElementById("hero").src='../../level/img/22350820.png';
@@ -78,8 +67,10 @@
         //規定レベルに達したらループ終了
         if(level.innerHTML >= currentLevel && lifeBar.value >= exExp){
           clearTimeout(gameTimer);        
+          // お褒めの言葉を表示
           document.getElementById("ohome_word").classList.add("is-show");
           
+          // レベルが上がった場合、桜の花びらを表示
           if(previousLevel != currentLevel){
             // コンテナを指定
             const section = document.querySelector('.cherry-blossom-container');
