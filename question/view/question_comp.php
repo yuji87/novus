bcd8fc2c6b92935595344d4678833098a16fbb5f
korie28 <Rieko.Terada@kusurinomadoguchi.co.jp>
2022-03-29 16:20:02 +0900

    少しずつマージしてみる

--- question/view/question_comp.php
+++ question/view/question_comp.php
@@ -39,21 +39,83 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="2.css" />
+  <link rel="stylesheet" type="text/css" href="../../css/mypage.css" />
+  <link rel="stylesheet" type="text/css" href="../../css/top.css" />
   <title>質問投稿完了</title>
 </head>
+
 <body>
+    <!--メニュー-->
+    <header>
+        <div class="navtext-container">
+            <div class="navtext">Q&A SITE</div>
+        </div>
+        <input type="checkbox" class="menu-btn" id="menu-btn">
+        <label for="menu-btn" class="menu-icon"><span class="navicon"></span></label>
+        <ul class="menu">
+            <li class="top"><a href="login_top.php">TOP Page</a></li>
+            <li><a href="../userEdit/edit_user.php">My Page</a></li>
+            <li><a href="#">TO DO LIST</a></li>
+            <li><a href="../../question/view/qhistory.php">質問 履歴</a></li>
+            <li><a href="../../">記事 履歴</a></li>
+            <li>
+                <form type="hidden" action="logout.php" method="POST">
+				    <input type="submit" name="logout" value="ログアウト" id="logout" style="text-align:left;">
+                </form>
+            </li>
+        </ul>
+    </header>
 
+    <!--コンテンツ-->
+    <section class="wrapper">
+        <div class="container">
+            <div class="content">
+                <p class="h4">投稿完了</p>
+                <p>以下の内容で投稿が完了しました</p>
+                <!--題名-->
+                <div class="fw-bold pb-1">題名</div>
+                <div><?php echo $hasTaken[0]['title'] ?></div>
+                <!--カテゴリー-->
+                <div class="fw-bold pt-3 pb-1">カテゴリ</div>
+                <div><?php echo $hasTaken[0]['category_name'] ?></div>
+                <!--本文-->
+                <div class="fw-bold pt-3 pb-1">本文</div>
+                <div><?php echo $hasTaken[0]['message'] ?></div>
+                <!--ファイルを投稿していたら表示-->
+                <?php if (isset($hasTaken[0]['question_image'])): ?>
+                  <img src="../../top/img/<?php echo $hasTaken[0]['question_image']; ?>">
+                <?php endif; ?> 
+                <form method="GET" name="form1" action="question_disp.php">
+                    <input type="hidden" name="question_id" value="<?php echo $hasTaken[0]['question_id']; ?>">
+                    <a href="javascript:form1.submit()" class="btn btn-warning mt-2">詳細画面へ</a>
+                </form>
+                <a href="../login_top.php" class="btn btn-outline-dark fw-bold">TOP</a>
+            </div>
+        </div>
+    </section>
 
-  <div>投稿完了</div>
-  <div>以下の内容で投稿が完了しました</div>
-    <div>題名：<?php echo $hasTaken[0]['title'] ?></div>
-    <div>カテゴリ：<?php echo $hasTaken[0]['category_name'] ?></div>
-    <div>本文：<?php echo $hasTaken[0]['message'] ?></div>
-
-    <form method="GET" name="form1" action="question_disp.php">
-      <input type="hidden" name="question_id" value="<?php echo $hasTaken[0]['question_id']; ?>">
-      <a href="javascript:form1.submit()">詳細画面へ</a>
-    </form>
-  <a href="../login_top.php">TOP</a>
+    <!-- フッタ -->
+    <footer class="h-10"><hr>
+		    <div class="footer-item text-center">
+		    	  <h4>Q&A SITE</h4>
+		    	  <ul class="nav nav-pills nav-fill">
+                <li class="nav-item">
+		    			  <a class="nav-link small" href="#">記事</a>
+		    		</li>
+		    		<li class="nav-item">
+		    			  <a class="nav-link small" href="#">質問</a>
+		    		</li>
+		    		<li class="nav-item">
+		    			  <a class="nav-link small" href="#">本検索</a>
+		    		</li>
+		    		<li class="nav-item">
+		    			  <a class="nav-link small" href="#">お問い合わせ</a>
+		    		</li>
+		    	</ul>
+		    </div>
+		    <p class="text-center small mt-2">Copyright (c) HTMQ All Rights Reserved.</p>
+	  </footer>
 </body>
 </html>
--- question/view/question_create.php
+++ question/view/question_create.php
@@ -55,49 +55,106 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="2.css" />
+    <link rel="stylesheet" type="text/css" href="../../css/mypage.css" />
+    <link rel="stylesheet" type="text/css" href="../../css/top.css" />
     <title>質問投稿ページ</title>
 </head>
 
 <body>
-    <div>質問したい内容を入力してください</div>
-    <!-- 質問投稿フォーム -->
-    <form method="POST" action="" name="q_data">
-        <div class=""style="text-align: center">
-            <div>
-                <?php if(isset($err['title'])): ?>
-                    <?php echo $err['title'] ?>
-                    <?php endif; ?>
-                </div>
-                <div>題名</div>
-                <input type="text" name="title"><br>
-                <div>
-                    <?php if(isset($err['category'])): ?>
-                        <?php echo $err['category'] ?>
-            <?php endif; ?>
+    <!--メニュー-->
+    <header>
+        <div class="navtext-container">
+            <div class="navtext">Q&A SITE</div>
         </div>
-        <div>カテゴリ</div>
-        <select name="category">
-            <option></option>
-            <?php foreach($categories as $value){ ?>
-                <option value="<?php echo $value['cate_id'] ?>"> 
-                    <?php echo $value['category_name'] ?>
-                </option>";
-            <?php } ?>
-        </select>
-        <br>
-        <div>
-            <?php if(isset($err['message'])): ?>
-                <?php echo $err['message'] ?>
-                <?php endif; ?>
+        <input type="checkbox" class="menu-btn" id="menu-btn">
+        <label for="menu-btn" class="menu-icon"><span class="navicon"></span></label>
+        <ul class="menu">
+            <li class="top"><a href="login_top.php">TOP Page</a></li>
+            <li><a href="../userEdit/edit_user.php">My Page</a></li>
+            <li><a href="#">TO DO LIST</a></li>
+            <li><a href="../../question/view/qhistory.php">質問 履歴</a></li>
+            <li><a href="../../">記事 履歴</a></li>
+            <li>
+                <form type="hidden" action="logout.php" method="POST">
+				    <input type="submit" name="logout" value="ログアウト" id="logout" style="text-align:left;">
+                </form>
+            </li>
+        </ul>
+    </header>
+
+    <section class="wrapper">
+        <div class="container">
+            <div class="content">
+                <p class="h4">質問投稿</p>
+                <p>質問したい内容を入力して下さい</P>
+                <!-- 質問投稿フォーム -->
+                <form method="POST" action="" name="q_data">
+                    <input type="hidden" name="user_id" value="<?php echo $_SESSION['login_user']['user_id']; ?>">
+                    <div class=""style="text-align: center">
+                    <!--題名-->
+                    <div class="fw-bold pb-1">題名</div>
+                    <input type="text" name="title"><br>
+                    <!--エラー表示-->
+                    <div>
+                        <?php if(isset($err['title'])): ?>
+                            <p class="text-danger pt-2"><?php echo $err['title'] ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <!--カテゴリー-->
+                    <div class="fw-bold pt-4 pb-1">カテゴリ</div>
+                    <select name="category">
+                        <option></option>
+                        <?php foreach($categories as $value){ ?>
+                            <option value="<?php echo $value['cate_id'] ?>"> 
+                                <?php echo $value['category_name'] ?>
+                            </option>";
+                        <?php } ?>
+                    </select>
+                    <!--エラー表示-->
+                    <div>
+                        <?php if(isset($err['category'])): ?>
+                            <p class="text-danger pt-2"><?php echo $err['category'] ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <!--本文-->
+                    <div class="fw-bold pt-4 pb-1">本文</div>
+                    <textarea name="message" rows="5" class="w-100"></textarea>
+                    <!--エラー表示-->
+                    <div>
+                        <?php if(isset($err['message'])): ?>
+                            <p class="text-danger pt-2"><?php echo $err['message'] ?></p>
+                        <?php endif; ?>
+                    </div>
+                    <!--添付ファイル-->
+                    <div class="fw-bold pt-4 pb-1">添付</div>
+                    <div class="small pb-3">※jpgまたはpng形式にてお願いいたします</div>
+                        <input type="file" name="question_image" accept="image/png, image/jpeg">
+                    <input type="submit" name="create_question" class="btn btn-warning mt-5 mb-5" value="投稿する">
+                </form>
             </div>
-        <div>本文</div>
-        <textarea name="message"></textarea>
-        <br>
-        <div>添付</div>
-        <div>※jpgもしくはpng形式にてお願いいたします。</div>
-        <input type="file" name="question_image" accept="image/png, image/jpeg">
-        <input type="hidden" name="user_id" value="<?php echo $_SESSION['login_user']['user_id']; ?>">
-        <input type="submit" name="create_question">
-    </form>
+        </div>
+    </section>
+
+    <!-- フッタ -->
+    <footer class="h-10"><hr>
+	    <div class="footer-item text-center">
+			<h4>Q&A SITE</h4>
+			<ul class="nav nav-pills nav-fill">
+                <li class="nav-item">
+					<a class="nav-link small" href="#">記事</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link small" href="#">質問</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link small" href="#">本検索</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link small" href="#">お問い合わせ</a>
+				</li>
+			</ul>
+		</div>
+		<p class="text-center small mt-2">Copyright (c) HTMQ All Rights Reserved.</p>
+	</footer>
 </body>
 </html>
