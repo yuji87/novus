86dc526c075cb870ea667d0879c71caa2bd079d9
Yuji Matsunaga <100896497+YujiMatsunaga@users.noreply.github.com>
2022-03-29 14:25:23 +0900

    14:25
    お願いします

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Qanda;
+
+define("DB_URL", "mysql:host=localhost;dbname=qandasite;charset=utf8mb4");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("LOCAL", "1");
+define("GMEMAIL", "support@hoge.jp");
+
+
