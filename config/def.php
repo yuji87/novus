4dd63d6fbbb017f7b78440979ff5e139d62d472e
Yuji Matsunaga <87655372+yuji87@users.noreply.github.com>
2022-04-04 15:22:41 +0900

    4/4これがいければ....!!

--- config/def.php
+++ config/def.php
@@ -1,13 +1,12 @@
 <?php
-
-namespace Qanda;
+namespace Novus;
 
 require_once "configA.php";
 
 // 定数
 define("VERSION", "1.0.0");
-define("SYSTITLE", "Qanda");
-define("DOMAIN", "/qandasite");
+define("SYSTITLE", "novus");
+define("DOMAIN", "/novus");
 
 define("LISTCOUNT", 5);
 define("LISTCOUNT_MYPAGE", 5);
